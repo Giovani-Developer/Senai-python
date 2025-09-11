af5b28d41af6cb7628b397387089b1d1707e2c5a
Giovani <giovanifreitasdc@gmail.com>
2025-09-10 21:52:09 -0300

    Aula de Operadores Aritiméticos

---
+++ src/teste.py
@@ -0,0 +1,32 @@
+# exec 1
+input("Descubra os resultados das contas com a senha de 123:  ")
+n1 = 15
+n2 = 27
+soma = n1 + n2
+
+print("O resultado é:", soma)
+
+# exec 2
+
+n1 = 12
+n2 = 8
+multiplicacao = n1 * n2
+print("O resultado é:", multiplicacao)
+
+# exec 3
+n1 = 25
+n2 = 4
+divisao = n1 // n2
+print("O resultado é:", divisao)
+
+# exec 4
+n1 = 37
+n2 = 5
+resto = n1 % n2
+print("O resultado é:", resto)
+
+# exec 5
+n1 = 3
+n2 = 4
+potencia = n1 ** n2
+print("O resultado é:", potencia)
---
+++ src/teste2.py
@@ -0,0 +1,26 @@
+
+print(10 > 7) # Retorna True pois 10 é maior que 7
+
+print(15 == 20) # Retorna False pois 15 não é igual a 20
+
+print(100 <= 100) # Retorna True pois 100 é igual a 100
+
+print(45 != 30) # Retorna o resultado True é diferente
+
+##################################################################
+
+print("Novo desafio abaixo: ")
+
+print(5 > 2 and 8 > 3)
+
+print(10 < 3 or 4 == 4)
+
+print (not 7 > 2)
+
+print("Novo desafio abaixo: ")
+
+x = 10
+x += 5
+x *= 2
+x -= 4
+print(x)
