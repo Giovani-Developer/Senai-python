4cc70eb7773c64317fbde35b831935d6805355bf
Giovani <giovanifreitasdc@gmail.com>
2025-09-15 21:50:39 -0300

    exercícios feitos, 15/09

--- src/app.py
+++ src/app.py
@@ -1,16 +1,72 @@
-from flask import Flask
-from dotenv import load_dotenv
-import os
+# idade = int(input("Digite sua idade: "))
 
-load_dotenv()
+# if idade <=13:
+    # print("criança")
+# elif idade <=17:
+    # print("Adolescente")
+# else:
+    # print("adulto")
 
+# contador = 1
 
-app = Flask(__name__)
+############################################################################################
 
-@app.route("/")
+# while contador <= 10:
+#     print("Contando...", contador)
+#     contador += 1
 
-def home():
-    return "Curso Backend"
+####################################################################################################
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# for i in range (2, 21):
+#     if i % 2 == 0:
+#         print(i)
+
+#################################################################################################
+
+# n1 = int(input("Coloque seu primeiro numero:" ))
+# n2 = int(input("Coloque seu segundo numero:" ))
+# n3 = int(input("Coloque seu terceiro numero:" ))
+# n4 = int(input("Coloque seu quarto numero:" ))
+# n5 = int(input("Coloque seu quinto numero:" ))
+
+# soma = n1 + n2 + n3 + n4 + n5 
+# print(soma)
+
+#################################################################################################
+
+# for i in range (1 , 11):
+#     i = i * 7
+#     print(i)
+
+####################################################################################################
+
+# soma = 0
+
+# for i in range(1, 10):
+#     n1 = int(input("Digite um numero: "))
+#     if n1 == 0:
+#         break   
+#     else:
+#         soma = soma + n1
+
+        
+# print("A soma total é de: ", soma)
+      
+###########################################################################################################
+
+# frutas = ["maça", "pera", "jaca"]
+# for fruta in frutas:
+#     print(fruta)
+
+# frutas.append("melao")
+
+# print(frutas)
+
+# lista = [1, 2]
+# lista.extend([2,4,5])
+# lista.remove(2)
+# print(lista)
+
+listas = [2, 5, 4, 2, 5, 6,]
+listas.sort()
+print(listas)
---
+++ src/codigo.py
@@ -0,0 +1,14 @@
+
+
+for i in range(1,10):
+    user = str(input("Quais itens voce deseja?"))
+    if user == ("sair"):
+        break
+
+
+listas_compras = []
+response = listas_compras
+
+print(response)
+
+
